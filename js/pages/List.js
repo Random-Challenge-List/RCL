f49bb66333d89e75fa26806fe2d2c925adef166f
Niiixk <74977717+Niiixk@users.noreply.github.com>
2025-07-17 12:30:08 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,7 +100,7 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without using hacks (however, CBF is allowed.)
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
@@ -122,6 +122,9 @@ export default {
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                    </p>
+                     <p>
+                        If a level has a CBF blocker, you can delete the CBF blocker but you will need to send the ID with the original ID of that level.
                     </p>
                 </div>
             </div>
